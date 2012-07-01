8f4b6e667e2e1f7c72cfe5bbd648973dce4adfd6
KingManuel <Hyo-Chung.Noguchi@uni-duesseldorf.de>
2012-07-01 02:49:47 +0200

    mapeditor fast fertig

--- src/game/bomberman/MapEditor.java
+++ src/game/bomberman/MapEditor.java
@@ -2,14 +2,17 @@ package game.bomberman;
 
 import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-public class MapEditor extends JFrame implements Runnable {
+public class MapEditor extends JFrame implements KeyListener, Runnable {
 
 	public static void main(String[] args) {
 
@@ -17,6 +20,8 @@ public class MapEditor extends JFrame implements Runnable {
 
 	}
 
+	private Thread t = new Thread(this);
+	private Pointer pointer;
 	private static final long serialVersionUID = 1L;
 	/**
 	 * Jeder Wert am Eintrag im Array (i,j) steht fuer ein Objekt an den
@@ -34,6 +39,7 @@ public class MapEditor extends JFrame implements Runnable {
 	private BufferedImage[] showimage = new BufferedImage[6];
 
 	public MapEditor() {
+		this.pointer = new Pointer();
 		this.setSize(528, 550);
 		int width = Toolkit.getDefaultToolkit().getScreenSize().width;
 		int height = Toolkit.getDefaultToolkit().getScreenSize().height;
@@ -57,11 +63,22 @@ public class MapEditor extends JFrame implements Runnable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		try {
+			this.showimage[2] = ImageIO.read(new File(System
+					.getProperty("user.dir") + "/pics/" + "ob2" + ".png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		t.start();
+		this.addKeyListener(this);
 
 	}
 
 	public void paint(Graphics g) {
-		g.drawString("I am a fucking Genious", 200, 520);
+		g.drawString(
+				"Press:   'a'  for  Path,   's'  for  Stone,   'd'  for  Box",
+				200, 520);
 		BufferedImage image = new BufferedImage(528, 500,
 				BufferedImage.TYPE_3BYTE_BGR);
 		// g2 uebernimmt image
@@ -75,6 +92,8 @@ public class MapEditor extends JFrame implements Runnable {
 
 			}
 		}
+		g2.drawImage(pointer.getShowimage(), pointer.getX(), pointer.getY(),
+				this);
 		g.drawImage(image, 0, 0, this);
 
 	}
@@ -94,4 +113,159 @@ public class MapEditor extends JFrame implements Runnable {
 		}
 	}
 
+	public void save() {
+		String savePath = System.getProperty("user.dir") + "/src/";
+		File file = new File(savePath + "RandomMap.xml");
+		try {
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file);
+			// als XML speichern
+			String s = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n";
+			s = s + "<config>\r\n";
+			for (int i = 0; i < 10; i++) {
+
+				for (int j = 0; j < 10; j++) {
+
+					if (map[i][j] == 2) {
+						s = s + "<ob id=\"box\"x=\"" + j * 48 + "\"y=\""
+								+ (i * 48 + 20) + "\"/>\r\n";
+					}
+					if (map[i][j] == 1) {
+						s = s + "<ob id=\"stone\"x=\"" + i * 48 + "\"y=\""
+								+ (j * 48 + 20) + "\"/>\r\n";
+					}
+
+				}
+			}
+			s = s + "</config>";
+			fw.write(s + "\r\n");
+			System.out.println(s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private class Pointer {
+		private int x;
+		private int y;
+		private BufferedImage showimage;
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		public BufferedImage getShowimage() {
+			return showimage;
+		}
+
+		public void setShowimage(BufferedImage showimage) {
+			this.showimage = showimage;
+		}
+
+		public Pointer() {
+			this.x = 66;
+			this.y = 38;
+			try {
+				this.showimage = ImageIO.read(new File(System
+						.getProperty("user.dir")
+						+ "/pics/"
+						+ "pointer"
+						+ ".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		public void leftmove() {
+			if (this.x > 96)
+				this.x = this.getX() - 48;
+		}
+
+		public void rightmove() {
+			if (this.getX() < 480) {
+				this.setX(this.getX() + 48);
+			}
+		}
+
+		public void upmove() {
+			if (this.y > 68)
+				this.y += -48;
+		}
+
+		public void downmove() {
+			if (this.y < 452)
+				this.y += 48;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent ke) {
+		// TODO Auto-generated method stub
+		// wenn man Pfeil nach rechts drueckt rightmove -->
+		if (ke.getKeyCode() == 39) {
+
+			this.pointer.rightmove();
+		}
+		// wenn man Pfeil nach links drueckt leftmove
+		if (ke.getKeyCode() == 37) {
+
+			this.pointer.leftmove();
+		}
+		// wenn man Pfeil nach unten drueckt downmove
+		if (ke.getKeyCode() == 40) {
+
+			this.pointer.downmove();
+		}
+		// wenn man Pfeil nach oben drueckt upmove
+		if (ke.getKeyCode() == 38) {
+
+			this.pointer.upmove();
+		}
+		// wenn man "d" drueckt setze Box
+		if (ke.getKeyCode() == 68) {
+
+			this.map[(this.pointer.getX() - 66) / 48][(this.pointer.getY() - 38) / 48] = 2;
+		}
+		// wenn man "a" drueckt setze Weg
+		if (ke.getKeyCode() == 65) {
+
+			this.map[(this.pointer.getX() - 66) / 48][(this.pointer.getY() - 38) / 48] = 0;
+		}
+		// wenn man "s" drueckt setze Stein
+		if (ke.getKeyCode() == 83) {
+
+			this.map[(this.pointer.getX() - 66) / 48][(this.pointer.getY() - 38) / 48] = 1;
+		}
+		if (ke.getKeyCode() == 113) {
+			this.save();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
